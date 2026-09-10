42984c9192480b412bd14915641f45ac754fd5b0
Jonas Welt <57290928+JW-200@users.noreply.github.com>
2026-09-10 14:02:35 +0200

    update headline size, change 2x card icons

--- dist/glow-cards.js
+++ dist/glow-cards.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '16.1.5';
+  const VERSION = '16.1.8';
   const stylesheetUrl = new URL(`./glow-card.css?v=${VERSION}`, import.meta.url);
   const devRevision = new URL(import.meta.url).searchParams.get('dev');
   if (devRevision) stylesheetUrl.searchParams.set('dev', devRevision);
